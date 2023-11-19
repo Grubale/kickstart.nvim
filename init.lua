76566e8aded56addac00bbea8bb826ba90a2882e
Fabian Gruber <f.gruber@limesoda.com>
2023-11-19 13:15:15 +0100

    Add typoscript & tsconfig icons and syntax highlighting

--- init.lua
+++ init.lua
@@ -55,6 +55,24 @@ require('lazy').setup({
       show_trailing_blankline_indent = false,
     },
   },
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    opts = {
+      override = {
+        tsconfig = {
+          icon = '',
+          color = '#8c8c8c',
+          name = 'TSconfig',
+        },
+        typoscript = {
+          icon = '',
+          color = '#FF8700',
+          name = 'TypoScript',
+        },
+      },
+    },
+  },
 }, {})
 
 -- [[ Setting options ]]
